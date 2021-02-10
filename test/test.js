c4136e6d4532a6b873d75fddc5fc3dce780b01b6
kmeigcasan <73453778+kmeigcasan@users.noreply.github.com>
2021-02-10 14:25:59 +0800

    Intentional fail

--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ describe("Numbers Model", function(){
     //Testing jenkins autobuild pr
     it("Return the expected return value on success", function(){
         let num = new Numbers();
-        let param = "1,2,3,4,5,6";
+        let param = "2,3,4,5,6";
         let result = num.getEven(param);
         console.log(result);
         expect(result).to.deep.equal(["2","4","6"]);
